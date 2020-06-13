c14b8e94013363cb9136738e9383174994a448c2
Junaid Maqsood <junaidmaqsood@live.com>
2020-06-13 16:41:20 +0500

    Shell Codebase Added

---
+++ server/shell/server.bash
@@ -0,0 +1,7 @@
+curl -d '{
+"client_id":"YOUR_CLIENT_ID",
+"client_secret":"YOUR_CLIENT_SECRET",
+"grant_type":"client_credentials",
+"audience":"https://topups.reloadly.com"
+	}' \
+-H "Content-Type: application/json" \
